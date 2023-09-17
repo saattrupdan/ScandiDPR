42eb3167e7037e499b3794ff41922315aae8f498
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-09-17 16:47:20 +0200

    fix: Adjust `num_training_steps`

--- src/scandi_dpr/train.py
+++ src/scandi_dpr/train.py
@@ -73,13 +73,11 @@ def train(
         betas=(cfg.first_momentum, cfg.second_momentum),
         weight_decay=cfg.weight_decay,
     )
-    num_optimization_steps_per_epoch: int = ceil(
-        len(train_dataloader) / (cfg.batch_size * cfg.gradient_accumulation_steps)
-    )
+    num_batches: int = ceil(len(train_dataloader) / cfg.batch_size)
     scheduler = get_cosine_schedule_with_warmup(
         optimizer=optimizer,
         num_warmup_steps=cfg.num_warmup_steps,
-        num_training_steps=cfg.num_epochs * num_optimization_steps_per_epoch,
+        num_training_steps=cfg.num_epochs * num_batches,
     )
     accelerator = Accelerator(
         gradient_accumulation_steps=cfg.gradient_accumulation_steps,
