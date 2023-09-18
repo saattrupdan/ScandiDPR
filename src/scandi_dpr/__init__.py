8bb6259074fceed6bbd8e2458825d572fa45b05c
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-09-18 10:42:46 +0200

    feat: Add saving the model and pushing to hub

--- src/scandi_dpr/__init__.py
+++ src/scandi_dpr/__init__.py
@@ -5,8 +5,9 @@
 import importlib.metadata
 from .data import load_data
 from .tokenization import tokenize_dataset
-from .model import load_model
+from .model import load_model, save_model
 from .train import train
+from .evaluate import evaluate
 
 # Fetches the version of the package as defined in pyproject.toml
 __version__ = importlib.metadata.version(__package__)
--- src/scandi_dpr/model.py
+++ src/scandi_dpr/model.py
@@ -1,5 +1,6 @@
 """Model definition of dense passage retrieval models."""
 
+from pathlib import Path
 from omegaconf import DictConfig
 from transformers import DPRContextEncoder, DPRQuestionEncoder
 import logging
@@ -35,3 +36,36 @@ def load_model(cfg: DictConfig) -> tuple[DPRContextEncoder, DPRQuestionEncoder]:
 
     logger.info(f"Loaded models from pretrained model ID {cfg.pretrained_model_id!r}")
     return context_encoder, question_encoder
+
+
+def save_model(
+    context_encoder: DPRContextEncoder,
+    question_encoder: DPRQuestionEncoder,
+    cfg: DictConfig,
+) -> None:
+    """Save the DPR model.
+
+    Args:
+        context_encoder: The context encoder.
+        question_encoder: The question encoder.
+        cfg: Hydra configuration.
+    """
+    logger.debug("Saving models")
+    with no_terminal_output():
+        model_dir = Path(cfg.dirs.models)
+        context_encoder_dir = model_dir / f"{cfg.model_name}-context-encoder"
+        question_encoder_dir = model_dir / f"{cfg.model_name}-question-encoder"
+        context_encoder.save_pretrained(context_encoder_dir)
+        question_encoder.save_pretrained(question_encoder_dir)
+    logger.info(f"Saved models to {model_dir!r}")
+
+    logger.debug("Pushing models to the Hugging Face Hub")
+    with no_terminal_output():
+        if cfg.push_to_hub:
+            context_encoder.push_to_hub(
+                repo_id=f"alexandrainst/{cfg.model_name}-context-encoder", token=True
+            )
+            question_encoder.push_to_hub(
+                repo_id=f"alexandrainst/{cfg.model_name}-question-encoder", token=True
+            )
+    logger.info("Pushed models to the Hugging Face Hub")
